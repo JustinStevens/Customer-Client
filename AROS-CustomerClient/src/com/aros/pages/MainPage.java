622917de42b3db64c125b33dcfc41c382db45cfc
JustinStevens <stevens.justin@hotmail.com>
2014-02-02 14:34:03 -0800

    Started creating main page

--- AROS-CustomerClient/src/com/aros/pages/MainPage.java
+++ AROS-CustomerClient/src/com/aros/pages/MainPage.java
@@ -1,12 +1,39 @@
 package com.aros.pages;
 
+import com.aros.customerclient.R;
+
+import android.app.Activity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainPage {
 	private RelativeLayout cLayout;
 	
-	public MainPage (RelativeLayout contentLayout)
+	public MainPage (Activity a, RelativeLayout contentLayout)
 	{
 		this.cLayout = contentLayout;
+		ImageView ad = new ImageView(a);
+		ad.setBackgroundResource(R.drawable.ad);
+		ad.setLayoutParams(new LayoutParams(400, 400));
+		this.cLayout.addView(ad);
+		ad = new ImageView(a);
+		ad.setBackgroundResource(R.drawable.ad);
+		ad.setLayoutParams(new LayoutParams(400, 400));
+		ad.setX(400);
+		this.cLayout.addView(ad);
+		
+		ad = new ImageView(a);
+		ad.setBackgroundResource(R.drawable.ad);
+		ad.setLayoutParams(new LayoutParams(400, 400));
+		ad.setY(400);
+		this.cLayout.addView(ad);
+		
+		ad = new ImageView(a);
+		ad.setBackgroundResource(R.drawable.ad);
+		ad.setLayoutParams(new LayoutParams(400, 400));
+		ad.setX(400);
+		ad.setY(400);
+		this.cLayout.addView(ad);
 	}
 }
